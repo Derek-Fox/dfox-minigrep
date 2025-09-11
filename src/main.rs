29473fcb6e2f6392245ad527ecec1cd2f03e5722
Derek Fox <derekefox@gmail.com>
2025-09-11 18:28:26 -0400

    Remove unneeded Result  from fn build() in Config (and rename to fn new()).

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::{Arg, ArgMatches, Command};
-use minigrep::{OutputFlags, SearchFlags, format_line, search};
+use minigrep::{OutputFlags, SearchFlags};
 use std::{
     error::Error,
     fs,
@@ -39,10 +39,7 @@ fn main() {
         )
         .get_matches();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
+    let config = Config::new(&args);
 
     if let Err(e) = run(config) {
         eprintln!("Application error: {e}");
@@ -61,7 +58,7 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     let query_len = config.query.len();
 
-    let matched_lines = search(&config.query, &contents, &config.search_flags);
+    let matched_lines = minigrep::search(&config.query, &contents, &config.search_flags);
 
     if config.output_flags.quiet {
         return Ok(());
@@ -70,7 +67,7 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
     for matched_line in matched_lines {
         println!(
             "{}",
-            format_line(&config.output_flags, matched_line, query_len)
+            minigrep::format_line(&config.output_flags, matched_line, query_len)
         );
     }
 
@@ -85,8 +82,8 @@ struct Config {
 }
 
 impl Config {
-    pub fn build(args: &ArgMatches) -> Result<Self, &'static str> {
-        Ok(Config {
+    pub fn new(args: &ArgMatches) -> Self {
+        Config {
             query: args.get_one::<String>("query").unwrap().clone(),
             file_path: args
                 .get_one::<String>("file")
@@ -100,6 +97,6 @@ impl Config {
                 args.get_flag("quiet"),
             ),
             search_flags: SearchFlags::new(args.get_flag("case-insensitive")),
-        })
+        }
     }
 }
